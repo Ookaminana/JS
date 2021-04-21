4fb17acd71a724f4fccd2efd1ca53abf67ee0d2a
Natali <ookaminiwork@gmail.com>
2021-04-21 17:08:26 +0200

    Удалила лишние строчки

--- NodeJS/demo/fs.js
+++ NodeJS/demo/fs.js
@@ -1,6 +1,5 @@
 //File System
 const fs = require('fs')
-const { error } = require('node:console')
 const path = require('path')
 
 // fs.mkdir(path.join(__dirname,'test'),(err)=>{
@@ -13,7 +12,7 @@ const path = require('path')
 
 const filePath = path.join(__dirname,'test','text.txt')
 
-fs.writeFile(filePath, 'Hello NodeJS!',(err)=>{
+fs.writeFile(filePath, 'Hello NodeJS!',err=>{
     if (err){
         throw err
     }
